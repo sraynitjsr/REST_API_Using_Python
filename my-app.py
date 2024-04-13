ebd7f2bd323e5b3026754785197b569f370f5755
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-14 02:53:38 +0530

    CRUD REST API Using Python Flask

--- my-app.py
+++ my-app.py
@@ -1,1 +1,63 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse, abort
 
+app = Flask(__name__)
+api = Api(app)
+
+students = {}
+
+next_student_id = 1
+
+def generate_student_id():
+    global next_student_id
+    current_id = next_student_id
+    next_student_id += 1
+    return current_id
+
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('age')
+parser.add_argument('grade')
+
+class StudentList(Resource):
+    def get(self):
+        return students
+
+    def post(self):
+        args = parser.parse_args()
+        student_id = generate_student_id()
+        students[student_id] = {
+            'name': args['name'],
+            'age': args['age'],
+            'grade': args['grade']
+        }
+        return students[student_id], 201
+
+class Student(Resource):
+    def get(self, student_id):
+        if student_id not in students:
+            abort(404, message="Student {} not found".format(student_id))
+        return students[student_id]
+
+    def put(self, student_id):
+        if student_id not in students:
+            abort(404, message="Student {} not found".format(student_id))
+        args = parser.parse_args()
+        students[student_id] = {
+            'name': args['name'],
+            'age': args['age'],
+            'grade': args['grade']
+        }
+        return students[student_id], 200
+
+    def delete(self, student_id):
+        if student_id not in students:
+            abort(404, message="Student {} not found".format(student_id))
+        del students[student_id]
+        return '', 204
+
+api.add_resource(StudentList, '/students')
+api.add_resource(Student, '/students/<int:student_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
